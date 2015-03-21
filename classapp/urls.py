839e3d318971f93d5dc55cba85c51239e517f08a
Paul <kiripaul@gmail.com>
2015-03-21 10:43:04 -0400

    stuff

--- classapp/urls.py
+++ classapp/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'classapp/login.html'}),
-    url(r'^accounts/logout/$', 'classapp.views.logout_view'),
+    url(r'^accounts/logout/$', 'classapp.views.logout_view', {'template_name': 'classapp/login.html'}),
     url(r'^accounts/profile/$', 'classapp.views.course_list_view'),
     url(r'^courses/list/$', 'classapp.views.course_list_view'),
     url(r'^courses/add/$', 'classapp.views.course_add_view'),
