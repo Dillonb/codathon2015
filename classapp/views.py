5b85b787b1d56b883c988cd9799ebfc3d110f2ec
Dillon Beliveau <dillonbeliveau@gmail.com>
2015-03-21 09:44:16 -0400

    Viewing courses

--- classapp/views.py
+++ classapp/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from classapp.models import *
 
 def home_view(request):
 	return render(request, "classapp/home.html")
 
 def logout_view(request):
     logout(request)
+
+@login_required
+def course_list_view(request):
+    courses = request.user.course_set.all()
+    return render(request,"classapp/courselist.html",{"courses": courses})
+
 
 # def profile_card_view(request):
 # 	return render(request, "classapp/")
