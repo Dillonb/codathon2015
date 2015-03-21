037f79b62e3c942bece0f851cffa3a7a5680494c
Dillon Beliveau <dillonbeliveau@gmail.com>
2015-03-21 09:28:49 -0400

    Added a logout view

--- classapp/views.py
+++ classapp/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+
 def home_view(request):
 	return render(request, "classapp/home.html")
 
 def login_logout_view(request):
 	return render(request, "classapp/login.html")
+
+def logout_view(request):
+    logout(request)
 
 # def profile_card_view(request):
 # 	return render(request, "classapp/")
