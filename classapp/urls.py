c2f0cf24f633f779d6aaa7e5e0a93c8dceaf82d8
Dillon Beliveau <dillonbeliveau@gmail.com>
2015-03-21 09:22:00 -0400

    Fixed root url

--- classapp/urls.py
+++ classapp/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'classapp/login.html'}),
-    url(r'^', 'django.contrib.auth.views.login', {'template_name': 'classapp/login.html'}),
+    url(r'^$', 'django.contrib.auth.views.login', {'template_name': 'classapp/login.html'}),
 )
