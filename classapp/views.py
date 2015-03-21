82c37e81a3bb52e489a9031662a5869067bb6c38
Paul <kiripaul@gmail.com>
2015-03-21 09:32:46 -0400

    Changing views :deleted login and changing base.html

--- classapp/views.py
+++ classapp/views.py
@@ -1,9 +1,6 @@
 from django.shortcuts import render
 def home_view(request):
 	return render(request, "classapp/home.html")
-
-def login_logout_view(request):
-	return render(request, "classapp/login.html")
-
+	
 # def profile_card_view(request):
 # 	return render(request, "classapp/")
